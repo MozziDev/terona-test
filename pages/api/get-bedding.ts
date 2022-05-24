42bc81025d876e2b2d03080c1cb1d574d5a3af19
MozziDev <maleevea@gmail.com>
2022-05-24 13:10:19 +0500

    Build

--- pages/api/get-bedding.ts
+++ pages/api/get-bedding.ts
@@ -12,9 +12,10 @@ export default async function handler(
             res.status(500).json({message: "Empty Bedding's"})
         }
         res.status(200).json(bedding)
+    }else{
+        res.status(405).json({message: "Only GET methods"})
     }
 
-    res.status(405).json({message: "Only GET methods"})
 
 
 }
